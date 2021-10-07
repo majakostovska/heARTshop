41300295465c1a21c22e6631aa5ad88570f3568c
majakostovska <74429535+majakostovska@users.noreply.github.com>
2021-10-07 17:24:36 +0200

    -repository-

---
+++ heARTshop.Repository/ApplicationDbContext.cs
@@ -0,0 +1,73 @@
+﻿
+using heARTshop.Domain.DomainModels;
+using heARTshop.Domain.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace heARTshop.Repository
+{
+    public class ApplicationDbContext : IdentityDbContext<heARTshopApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public virtual DbSet<ProductInShoppingCart> ProductInShoppingCarts { get; set; }
+        public virtual DbSet<ProductInOrder> ProductInOrders { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<EmailMessage> EmailMessages { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Product>()
+                .Property(z => z.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Entity<ShoppingCart>()
+                .Property(z => z.Id)
+                .ValueGeneratedOnAdd();
+
+            //builder.Entity<ProductInShoppingCart>()
+            //    .HasKey(z => new { z.ProductId, z.ShoppingCartId });
+
+            builder.Entity<ProductInShoppingCart>()
+                .HasOne(z => z.Product)
+                .WithMany(z => z.ProductInShoppingCarts)
+                .HasForeignKey(z => z.ShoppingCartId);
+
+            builder.Entity<ProductInShoppingCart>()
+                .HasOne(z => z.ShoppingCart)
+                .WithMany(z => z.ProductInShoppingCarts)
+                .HasForeignKey(z => z.ProductId);
+
+
+            builder.Entity<ShoppingCart>()
+                .HasOne<heARTshopApplicationUser>(z => z.Owner)
+                .WithOne(z => z.UserCart)
+                .HasForeignKey<ShoppingCart>(z => z.OwnerId);
+
+
+            //builder.Entity<ProductInOrder>()
+            //   .HasKey(z => new { z.ProductId, z.OrderId });
+
+            builder.Entity<ProductInOrder>()
+                .HasOne(z => z.OrderedProduct)
+                .WithMany(z => z.Orders)
+                .HasForeignKey(z => z.OrderId);
+
+            builder.Entity<ProductInOrder>()
+                .HasOne(z => z.UserOrder)
+                .WithMany(z => z.ProductInOrders)
+                .HasForeignKey(z => z.ProductId);
+        }
+    }
+}
