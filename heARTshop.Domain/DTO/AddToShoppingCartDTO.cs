f9e9beaafb906d0408eec6ab1739def447a893e5
majakostovska <74429535+majakostovska@users.noreply.github.com>
2021-10-07 17:16:08 +0200

    Add files via upload

---
+++ heARTshop.Domain/DTO/AddToShoppingCartDTO.cs
@@ -0,0 +1,16 @@
+﻿
+using heARTshop.Domain.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace heARTshop.Domain.DTO
+{
+    public class AddToShoppingCartDTO
+    {
+            public Product SelectedProduct { get; set; }
+            public Guid ProductId { get; set; }
+            public int Quantity { get; set; }
+    }
+}
---
+++ heARTshop.Domain/DTO/ShoppingCartDTO.cs
@@ -0,0 +1,15 @@
+﻿
+using heARTshop.Domain.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace heARTshop.Domain.DTO
+{
+    public class ShoppingCartDTO
+    {
+        public List<ProductInShoppingCart> ProductInShoppingCart { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
