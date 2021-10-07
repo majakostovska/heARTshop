97eb21edb9feb4db66462e08e3092677578f9bc2
majakostovska <74429535+majakostovska@users.noreply.github.com>
2021-10-07 17:30:30 +0200

    --

---
+++ heARTshop.Services/Implementation/BackgroundEmailSender.cs
@@ -0,0 +1,30 @@
+﻿using heARTshop.Domain.DomainModels;
+using heARTshop.Repository.Interface;
+using heARTshop.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heARTshop.Services.Implementation
+{
+    public class BackgroundEmailSender : IBackgroundEmailSender
+    {
+        private readonly IEmailService _emailService;
+        private readonly IRepository<EmailMessage> _mailRepository;
+
+
+        public BackgroundEmailSender(IEmailService emailService, IRepository<EmailMessage> mailRepository)
+        {
+            _emailService = emailService;
+            _mailRepository = mailRepository;
+        }
+
+        public async Task DoWork()
+        {
+
+            await _emailService.SendEmailAsync(_mailRepository.GetAll().Where(z => !z.Status).ToList());
+        }
+    }
+}
---
+++ heARTshop.Services/Implementation/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using heARTshop.Domain.DomainModels;
+using heARTshop.Repository.Interface;
+using heARTshop.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace heARTshop.Services.Implementation
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IOrderRepository _orderRespository;
+        public OrderService(IOrderRepository orderRespository)
+        {
+            this._orderRespository = orderRespository;
+        }
+        public List<Order> getAllOrders()
+        {
+            return this._orderRespository.getAllOrders();
+        }
+
+        public Order GetOrderDetails(BaseEntity model)
+        {
+            return this._orderRespository.GetOrderDetails(model);
+        }
+    }
+}
