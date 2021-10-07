58cbe54fa4d174987a1242f7b623502e83611119
majakostovska <74429535+majakostovska@users.noreply.github.com>
2021-10-07 17:15:10 +0200

    Add files via upload

---
+++ heARTshop.Domain/DomainModels/Order.cs
@@ -0,0 +1,18 @@
+﻿using heARTshop.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace heARTshop.Domain.DomainModels
+{
+    public class Order : BaseEntity
+    {
+        public Guid Id { get; set; }
+            public string UserId { get; set; }
+            public heARTshopApplicationUser User { get; set; }
+            public IEnumerable<ProductInOrder> ProductInOrders { get; set; }
+
+        
+    }
+}
