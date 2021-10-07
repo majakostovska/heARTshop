c73c52c67ca10b9981b2d52488807858a34b2d71
majakostovska <74429535+majakostovska@users.noreply.github.com>
2021-10-07 18:17:04 +0200

    Add files via upload

---
+++ heARTshop.Web/Controllers/Api/AdminController.cs
@@ -0,0 +1,71 @@
+﻿using heARTshop.Domain.DomainModels;
+using heARTshop.Domain.Identity;
+using heARTshop.Services.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace heARTshop.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        private readonly UserManager<heARTshopApplicationUser> userManager;
+        public AdminController(IOrderService orderService, UserManager<heARTshopApplicationUser> userManager)
+        {
+            this._orderService = orderService;
+            this.userManager = userManager;
+        }
+
+        [HttpGet("[action]")]
+        public List<Order> GetOrders()
+        {
+            return this._orderService.getAllOrders();
+        }
+
+        [HttpPost("[action]")]
+        public Order GetDetailsForProduct(BaseEntity model)
+        {
+            return this._orderService.GetOrderDetails(model);
+        }
+
+        [HttpPost("[action]")]
+        public bool ImportAllUsers(List<UserRegistrationDTO> model)
+        {
+            bool status = true;
+
+            foreach (var item in model)
+            {
+                var userCheck = userManager.FindByEmailAsync(item.Email).Result;
+
+                if (userCheck == null)
+                {
+                    var user = new heARTshopApplicationUser
+                    {
+                        UserName = item.Email,
+                        NormalizedUserName = item.Email,
+                        Email = item.Email,
+                        EmailConfirmed = true,
+                        PhoneNumberConfirmed = true,
+                        UserCart = new ShoppingCart()
+                    };
+                    var result = userManager.CreateAsync(user, item.Password).Result;
+
+                    status = status && result.Succeeded;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return status;
+        }
+    }
+}
---
+++ heARTshop.Web/Controllers/ShoppingCartController.cs
@@ -0,0 +1,107 @@
+﻿
+using heARTshop.Domain.DomainModels;
+using heARTshop.Domain.DTO;
+using heARTshop.Domain.Identity;
+using heARTshop.Repository;
+using heARTshop.Services.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace heARTshop.Web.Controllers
+{
+    [Authorize]
+    public class ShoppingCartController : Controller
+    {
+
+        private readonly IShoppingCartService _shoppingCartService;
+
+        public ShoppingCartController(IShoppingCartService shoppingCartService)
+        {
+            _shoppingCartService = shoppingCartService;
+        }
+
+        public IActionResult Index()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return View(this._shoppingCartService.getShoppingCartInfo(userId));
+        }
+
+
+
+        public IActionResult DeleteProductFromShoppingCart(Guid id)
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var result = this._shoppingCartService.DeleteProductFromShoppingCart(userId, id);
+
+            if (result)
+            {
+                return RedirectToAction("Index", "ShoppingCart");
+            }
+            else
+            {
+                return RedirectToAction("Index", "ShoppingCart");
+            }
+        }
+
+        //private Boolean Order()
+        public IActionResult Order()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var result = this._shoppingCartService.Order(userId);
+
+            //return result;
+            return RedirectToAction("Index", "ShoppingCart");
+        }
+        
+        public IActionResult PayOrder(string stripeEmail, string stripeToken)
+        {
+            var customerService = new CustomerService();
+            var chargeService = new ChargeService();
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var order = this._shoppingCartService.getShoppingCartInfo(userId);
+
+            var customer = customerService.Create(new CustomerCreateOptions
+            {
+                Email = stripeEmail,
+                Source = stripeToken
+            });
+
+            var charge = chargeService.Create(new ChargeCreateOptions
+            {
+                Amount = (Convert.ToInt32(order.TotalPrice) * 100),
+                Description = "CinemaShop Application Payment",
+                Currency = "mkd",
+                Customer = customer.Id
+            });
+
+            if (charge.Status == "succeeded")
+            {
+                var result = this.Order();
+
+                if (result != null)
+                {
+                    return RedirectToAction("Index", "ShoppingCart");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "ShoppingCart");
+                }
+            }
+
+            return RedirectToAction("Index", "ShoppingCart");
+        }
+
+    }
+}
