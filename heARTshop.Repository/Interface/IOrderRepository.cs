44e57b307ddc92863548d53aee9ef74f7de59dea
majakostovska <74429535+majakostovska@users.noreply.github.com>
2021-10-07 17:22:53 +0200

    Add files via upload

---
+++ heARTshop.Repository/Interface/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using heARTshop.Domain.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace heARTshop.Repository.Interface
+{
+        public interface IOrderRepository
+        {
+            List<Order> getAllOrders();
+            Order GetOrderDetails(BaseEntity model);
+        }
+}
---
+++ heARTshop.Repository/Interface/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using heARTshop.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace heARTshop.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        IEnumerable<heARTshopApplicationUser> GetAll();
+        heARTshopApplicationUser Get(string id);
+        void Insert(heARTshopApplicationUser entity);
+        void Update(heARTshopApplicationUser entity);
+        void Delete(heARTshopApplicationUser entity);
+    }
+}
